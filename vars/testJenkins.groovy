f8d763e97b6d36088e0ab67bdd950e5191d32e47
Teddy Parker <57333590+Teddy-Parker1388@users.noreply.github.com>
2024-02-15 17:07:49 -0500

    Update testJenkins.groovy

--- vars/testJenkins.groovy
+++ vars/testJenkins.groovy
@@ -29,6 +29,7 @@ def commitPush(){
     withCredentials([usernamePassword(credentialsId: 'github-cred', passwordVariable: 'pass', usernameVariable: 'user')]) {
         sh  """
             git config --local user.name ${user}
+            git config --local user.password ${pass}
             git diff --quiet && git diff --staged --quiet || git commit -am 'Update various files'
             git push origin ${env.BRANCH_NAME}
             """
