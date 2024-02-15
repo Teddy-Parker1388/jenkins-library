d6db08eeb28074a63348deda84c9e9e85d7e83ae
tparker40267 <teddy.parker@cengage.com>
2024-02-15 14:47:26 -0500

    *update groovu

--- vars/testJenkins.groovy
+++ vars/testJenkins.groovy
@@ -1,8 +1,39 @@
 
+def environments = /^(dev|prod|qa|stage|perf).*/
+
 def call(){
- sh "echo Hello World"
- sh "echo ${env.BRANCH_NAME}"
+    def environments = /^(dev|prod|qa|stage|perf).*/
+
+    if (env.BRANCH_NAME ==~ environments){
+        stage("TAC SYNC"){
+            echo "Running `tsunami tac sync`"
+            tacSync()
+        }
+        stage("COMMIT CHANGES"){
+            echo "Commit any changes. Push to remote"
+            gitCommit()
+        }
+        stage("TAC VALIDATE"){
+            echo "Running `tsunami tac validate`"
+            tacValidate()
+
+        }
+    }
+
+}
 
 
+def tacSync() {
+    sh "tsunami tac sync"
+}
 
+def commitPush(){
+    sh """  
+    git diff --quiet && git diff --staged --quiet || git commit -am 'Update various files'
+    git push origin ${env.BRANCHNAME}
+    """
+}
+
+def tacValidate() {
+    sh "tsunami tac validate"
 }
